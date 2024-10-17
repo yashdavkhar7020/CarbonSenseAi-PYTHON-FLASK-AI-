a279710aacdda46d1836a0104036f31a6a011aa4
Yash Shivaji davkhar <yashdavkhar007@gmail.com>
2024-10-17 19:00:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-we have claimed a patent for this project so we cant display necessary data/codes.
+we have claimed a patent for this project so we cant display necessary data/codes but u can get information about this project in readme file.
